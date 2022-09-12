685dec7639c123612c156f155090d57fb494a5c4
Federico Mazzei <federicomazzei@gmail.com>
2022-09-12 14:58:43 -0300

    Comento las lineas con sus acciones

--- webscraping-tables.py
+++ webscraping-tables.py
@@ -1,29 +1,38 @@
 #!/usr/bin/env python3 #Esta linea no se debe borrar
 #  -*- coding: utf-8 -*-
 import requests
+import time
 from bs4 import BeautifulSoup
-from plyer import notification
+from win10toast import ToastNotifier
 
+# Obtengo la url del sitio a obtener los datos
 url = 'https://www.bna.com.ar/Personas'
-icoDolar = '/home/fideo/proyectos/notifications/assets/images/dolar.ico'
 
+# Declaro el path a un icono de dolar
+icoDolar = './assets/images/dolar.ico'
+
+# A traves de request obtengo todo el codigo de la url pasada
 r = requests.get(url)
+# Paso a BeautifulSoup lo obtenido como texto y lo parseo como HTML
 soup = BeautifulSoup(r.text, 'html.parser')
 
+# Busco el tag table con class table cotizacion
 tabla_dolar = soup.find('table', class_= 'table cotizacion')
 
+# Recorro la tabla encontrando los tags tbody
 for cotizacion in tabla_dolar.find_all('tbody'):
   dolar = cotizacion.find_all('td')[2].text
 
+# Abro el archivo valorDolar.txt donde almaceno el valor dolar.
 valorArchivo = open('./valorDolar.txt', "r+")
 cotizacionActual=valorArchivo.read(10).strip("\n")
 
+# Pregunto si la cotizacionActual es distinta a la del dolar obtenido del sitio.
 if (dolar != cotizacionActual):
-  notification.notify(
-              title = 'Dolar',
-              message = dolar,
-              timeout = 20,
-              app_icon = icoDolar
-      )
+  toaster = ToastNotifier()
+  toaster.show_toast("La cotizacion del Dolar es:",
+            dolar,
+            icon_path=icoDolar,
+            duration=10)
   valorArchivo.seek(0, 0)
   valorArchivo.write(dolar)
