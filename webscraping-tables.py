b16ff8a1839d3fabffa2cd7e3a0c663d599f3b71
Federico Mazzei <federicomazzei@gmail.com>
2022-09-12 18:15:56 -0300

    agrego Notification

--- webscraping-tables.py
+++ webscraping-tables.py
@@ -4,6 +4,7 @@ import requests
 import time
 from bs4 import BeautifulSoup
 from win10toast import ToastNotifier
+from winotify import Notification, audio
 
 # Obtengo la url del sitio a obtener los datos
 url = 'https://www.bna.com.ar/Personas'
@@ -29,10 +30,20 @@ cotizacionActual=valorArchivo.read(10).strip("\n")
 
 # Pregunto si la cotizacionActual es distinta a la del dolar obtenido del sitio.
 if (dolar != cotizacionActual):
-  toaster = ToastNotifier()
+  """ toaster = ToastNotifier()
   toaster.show_toast("La cotizacion del Dolar es:",
             dolar,
             icon_path=icoDolar,
             duration=10)
+  """
+  toast = Notification(app_id="winotify test",
+                             title="Winotify Test Toast",
+                             msg="New Notification with actions!")
+  toast.add_actions("go to github", "https://github.com/versa-syahptr/winotify")
+ 
+  toast.show()
+  '''
+  toast.set_audio(audio.Mail, loop=False)
   valorArchivo.seek(0, 0)
-  valorArchivo.write(dolar)
+  valorArchivo.write(dolar) '''
+
