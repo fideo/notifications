7004ad048c4fecce3beb8fd32b5be1c568ab17bc
Federico Mazzei <federicomazzei@gmail.com>
2022-09-13 11:18:48 -0300

    notificaciones para Linux y para Windows

--- cotizacionDolar_Win.py
+++ cotizacionDolar_Win.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python3 #Esta linea no se debe borrar
 #  -*- coding: utf-8 -*-
 import requests
-import time
 from bs4 import BeautifulSoup
-from win10toast import ToastNotifier
 from winotify import Notification, audio
 
 # Obtengo la url del sitio a obtener los datos
 url = 'https://www.bna.com.ar/Personas'
 
 # Declaro el path a un icono de dolar
-icoDolar = './assets/images/dolar.ico'
+icoDolar = './dolar.ico'
 
 # A traves de request obtengo todo el codigo de la url pasada
 r = requests.get(url)
@@ -29,21 +27,14 @@ valorArchivo = open('./valorDolar.txt', "r+")
 cotizacionActual=valorArchivo.read(10).strip("\n")
 
 # Pregunto si la cotizacionActual es distinta a la del dolar obtenido del sitio.
+# Si es distinta muestro la notificacion con el nuevo valor del dolar.
 if (dolar != cotizacionActual):
-  """ toaster = ToastNotifier()
-  toaster.show_toast("La cotizacion del Dolar es:",
-            dolar,
-            icon_path=icoDolar,
-            duration=10)
-  """
-  toast = Notification(app_id="winotify test",
-                             title="Winotify Test Toast",
-                             msg="New Notification with actions!")
-  toast.add_actions("go to github", "https://github.com/versa-syahptr/winotify")
- 
-  toast.show()
-  '''
-  toast.set_audio(audio.Mail, loop=False)
+  toast = Notification(app_id="Cambio la Cotizacion del Dolar",
+                       title="Nueva Cotizacion del Dolar",
+                       msg=dolar)
+  toast.set_audio(audio.SMS, loop=False)
+  toast.build().show()
   valorArchivo.seek(0, 0)
-  valorArchivo.write(dolar) '''
+  valorArchivo.write(dolar)
 
+valorArchivo.close()
