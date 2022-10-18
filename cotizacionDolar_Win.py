ad9615d3a14f561ab996de28871ee3391f204484
Federico Mazzei <federicomazzei@gmail.com>
2022-10-18 15:15:44 -0300

    actualizacion según hora

--- cotizacionDolar_Win.py
+++ cotizacionDolar_Win.py
@@ -17,10 +17,29 @@ soup = BeautifulSoup(r.text, 'html.parser')
 
 # Busco el tag table con class table cotizacion
 tabla_dolar = soup.find('table', class_= 'table cotizacion')
+hora = soup.find('div', class_= 'legal')
+hora = hora.contents[0]
 
 # Recorro la tabla encontrando los tags tbody
 for cotizacion in tabla_dolar.find_all('tbody'):
   dolar = cotizacion.find_all('td')[2].text
+  #print(dolar)
+
+# Abro el archivo valorDolar.txt donde almaceno el valor dolar.
+horaArchivo = open('./horaDolar.txt', "r+")
+horaActual=horaArchivo.read(20).strip("\n")
+
+if (hora != horaActual):
+  toast = Notification(app_id="Se actualizó el Dolar",
+                       title="Actualización Hora / Dolar",
+                       msg=dolar + '\n' + hora)
+  toast.set_audio(audio.SMS, loop=False)
+  toast.build().show()
+  horaArchivo.seek(0, 0)
+  horaArchivo.write(hora)
+
+horaArchivo.close()
+
 
 # Abro el archivo valorDolar.txt donde almaceno el valor dolar.
 valorArchivo = open('./valorDolar.txt', "r+")
@@ -29,12 +48,14 @@ cotizacionActual=valorArchivo.read(10).strip("\n")
 # Pregunto si la cotizacionActual es distinta a la del dolar obtenido del sitio.
 # Si es distinta muestro la notificacion con el nuevo valor del dolar.
 if (dolar != cotizacionActual):
-  toast = Notification(app_id="Cambio la Cotizacion del Dolar",
-                       title="Nueva Cotizacion del Dolar",
-                       msg=dolar)
-  toast.set_audio(audio.SMS, loop=False)
-  toast.build().show()
-  valorArchivo.seek(0, 0)
-  valorArchivo.write(dolar)
+   #toast = Notification(app_id="Cambio la Cotizacion del Dolar",
+   #                     title="Nueva Cotizacion del Dolar",
+   #                     msg=dolar)
+   #toast.set_audio(audio.SMS, loop=False)
+   #toast.build().show()
+   valorArchivo.seek(0, 0)
+   valorArchivo.write(dolar)
 
 valorArchivo.close()
+
+
